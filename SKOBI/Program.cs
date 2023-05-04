e69e8f7d4ca762ef21d8448fb9c94066ec530e22
Ridiska34 <temich04om@mail.ru>
2023-05-04 23:27:36 +0600

    Добавьте файлы проекта.

---
+++ SKOBI/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SKOBI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите много скобок: ");
+            string skobi = Console.ReadLine().Replace(" ", "");
+
+
+            Stack<char> stack = new Stack<char>();
+            bool real = true;
+
+            foreach (var skob in skobi)
+            {
+                if (skob == '(' || skob == '{' || skob == '[')
+                {
+                    stack.Push(skob);
+                }
+                else if (stack.Count != 0)
+                {
+                    if ((skob == ')' && stack.Peek() != '(') || (skob == '}' && stack.Peek() != '{') || (skob == ']' && stack.Peek() != '['))
+                        real = false;
+                    else
+                        stack.Pop();
+                }
+                else
+                    real = false;
+                
+            }
+            
+            if (real)
+                Console.WriteLine("Всё идеально");
+            else
+                Console.WriteLine("Мда...Неверно!");
+
+            Console.ReadKey();
+        }
+    }
+}
